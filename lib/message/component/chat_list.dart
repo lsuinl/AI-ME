2a7663e3d1a1e1e6dc846df6ad8d1e9d6bb36b83
lsuinl <su96in43@naver.com>
2023-11-15 00:37:57 +0900

    feat: 스크롤 이동

--- lib/message/component/chat_list.dart
+++ lib/message/component/chat_list.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ChatList extends StatelessWidget {
   final List<Widget> widgets;
 
-  const ChatList({
-    required this.widgets,
-    super.key});
+  const ChatList({required this.widgets, super.key});
 
   @override
   Widget build(BuildContext context) {
+    final ScrollController _scrollController = ScrollController();
+    SchedulerBinding.instance!.addPostFrameCallback((_) {
+      _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+    });
     return SingleChildScrollView(
         physics: BouncingScrollPhysics(),
         clipBehavior: Clip.antiAliasWithSaveLayer,
+        controller: _scrollController,
         //shrinkWrap: true,
-        child: Column(children: widgets)
-    );
+        child: Column(children: [
+          Column(children: widgets),
+          SizedBox(height: 120.h)
+        ]));
   }
 }
